7e4f81aac4a1286bee8d0b76d6fd2f56fef7a5d2
lyna <lynabouharoun9@gmail.com>
2025-06-19 23:35:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -118,7 +118,7 @@
 
 document.getElementById('downloadBtn').addEventListener('click', function() {
   const link = document.createElement('a');
-  link.href = 'docs/White and Black Tech Professional Resume (1).pdf';  // chemin vers ton PDF
+  link.href = 'docs/MyCV.pdf';  // chemin vers ton PDF
   link.download = 'Lyna_CV.pdf';  // nom du fichier téléchargé
   document.body.appendChild(link);
   link.click();
